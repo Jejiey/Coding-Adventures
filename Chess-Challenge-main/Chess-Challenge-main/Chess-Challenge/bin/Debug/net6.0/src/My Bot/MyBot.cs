ac3a54c144848a3011b067c2f08ae8dee09cccfc
Jejiey <84489995+Jejiey@users.noreply.github.com>
2024-03-28 09:38:30 +1100

    Add files via upload

---
+++ Chess-Challenge-main/Chess-Challenge-main/Chess-Challenge/bin/Debug/net6.0/src/My Bot/MyBot.cs
@@ -0,0 +1,14 @@
+﻿using ChessChallenge.API;
+
+public class MyBot : IChessBot
+{
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] movesa = board.GetLegalMoves();
+        Move[] movesb = board.GetLegalMoves(true);
+        System.Diagnostics.Debug.WriteLine(movesb[0]);
+       // Move Do = movesb[0] == null? movesa[0] : movesb[0];
+
+        return movesa[0];
+    }
+}
